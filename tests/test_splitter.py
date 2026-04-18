540ae48074b83942cd160e002e3c5541f7e306ac
Kazuya Takei <myself@attakei.net>
2026-04-18 16:00:09 +0900

    ci: Use temporary directory in tests

--- tests/test_splitter.py
+++ tests/test_splitter.py
@@ -3,8 +3,8 @@
 from __future__ import annotations
 
 from importlib import metadata
+from pathlib import Path
 
-import platformdirs
 import pytest
 from atsphinx.lindera import splitter as t
 
@@ -34,13 +34,9 @@ def test_tokenize(text: str, tokens: list[str]):
 @pytest.mark.parametrize(
     "dict_type", ["ipadic", "ipadic-neologd", "cc-cedict", "jieba", "ko-dic", "unidic"]
 )
-def test_dictionary(dict_type: str):
+def test_dictionary(dict_type: str, tmp_path: Path):
     lindera_version = metadata.version("lindera-python")
-    cache_base_dir = platformdirs.user_cache_path("atsphinx-lindera")
-    sys_dict = t.SystemDictionary.init(dict_type)  # ty: ignore[invalid-argument-type]
-    assert (
-        sys_dict.local_path
-        == cache_base_dir / "_dict" / lindera_version / f"lindera-{dict_type}"
-    )
+    sys_dict = t.SystemDictionary(dict_type, lindera_version, tmp_path)  # ty: ignore[invalid-argument-type]
+    assert sys_dict.local_path == tmp_path / lindera_version / f"lindera-{dict_type}"
     sys_dict.fetch()
     assert sys_dict.local_path.exists()
